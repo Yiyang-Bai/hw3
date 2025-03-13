b04b5eacd01dce4f1b0be5a3be8b5fd91c2044d9
Ilovetheirishbeer <bai.yiy@northeastern.edu>
2025-03-13 13:43:11 -0400

    Initial commit - Upload HW3 website

---
+++ templete.js
@@ -0,0 +1,255 @@
+// Load the data
+const socialMedia = d3.csv("socialMedia.csv");
+
+socialMedia.then(function(data) {
+    data.forEach(function(d) {
+        d.Likes = +d.Likes;
+    });
+
+    const margin = {top: 50, right: 50, bottom: 70, left: 70};
+    const width = 800 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const svg = d3.select("#boxplot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const xScale = d3.scaleBand()
+        .domain([...new Set(data.map(d => d.Platform))])
+        .range([0, width])
+        .padding(0.3);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.Likes)])
+        .range([height, 0]);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
+
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .style("text-anchor", "middle")
+        .text("Platform");
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 20)
+        .style("text-anchor", "middle")
+        .text("Number of Likes");
+
+    const rollupFunction = function(groupData) {
+        const values = groupData.map(d => d.Likes).sort(d3.ascending);
+        return {
+            min: d3.min(values),
+            q1: d3.quantile(values, 0.25),
+            median: d3.quantile(values, 0.5),
+            q3: d3.quantile(values, 0.75),
+            max: d3.max(values)
+        };
+    };
+
+    const quantilesByGroups = d3.rollup(data, rollupFunction, d => d.Platform);
+
+    quantilesByGroups.forEach((quantiles, Platform) => {
+        const x = xScale(Platform);
+        const boxWidth = xScale.bandwidth();
+
+        svg.append("line")
+            .attr("x1", x + boxWidth / 2)
+            .attr("x2", x + boxWidth / 2)
+            .attr("y1", yScale(quantiles.min))
+            .attr("y2", yScale(quantiles.max))
+            .attr("stroke", "black")
+            .attr("stroke-width", 1);
+
+        svg.append("rect")
+            .attr("x", x)
+            .attr("y", yScale(quantiles.q3))
+            .attr("width", boxWidth)
+            .attr("height", yScale(quantiles.q1) - yScale(quantiles.q3))
+            .attr("fill", "steelblue")
+            .attr("stroke", "black")
+            .attr("stroke-width", 1);
+
+        svg.append("line")
+            .attr("x1", x)
+            .attr("x2", x + boxWidth)
+            .attr("y1", yScale(quantiles.median))
+            .attr("y2", yScale(quantiles.median))
+            .attr("stroke", "red")
+            .attr("stroke-width", 2)
+            .attr("stroke-dasharray", "4 4");
+    });
+});
+
+// Style for Bar Plot
+const socialMediaAvg = d3.csv("socialMediaAvg.csv");
+
+socialMediaAvg.then(function(data) {
+    data.forEach(function(d) {
+        d.AvgLikes = +d.AvgLikes;
+    });
+
+    const margin = {top: 80, right: 200, bottom: 70, left: 70};
+    const width = 800 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const svg = d3.select("#barplot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const x0 = d3.scaleBand()
+        .domain([...new Set(data.map(d => d.Platform))])
+        .range([0, width])
+        .padding(0.2);
+
+    const x1 = d3.scaleBand()
+        .domain([...new Set(data.map(d => d.PostType))])
+        .range([0, x0.bandwidth()])
+        .padding(0.05);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.AvgLikes) * 1.1])
+        .range([height, 0]);
+
+    const color = d3.scaleOrdinal()
+        .domain([...new Set(data.map(d => d.PostType))])
+        .range(["#ff6384", "#36a2eb", "#ffcd56"]);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x0));
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    const barGroups = svg.selectAll("bar")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("transform", d => `translate(${x0(d.Platform)},0)`);
+
+    barGroups.append("rect")
+        .attr("x", d => x1(d.PostType))
+        .attr("y", d => y(d.AvgLikes))
+        .attr("width", x1.bandwidth())
+        .attr("height", d => height - y(d.AvgLikes))
+        .attr("fill", d => color(d.PostType))
+        .attr("rx", 5);
+
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .style("text-anchor", "middle")
+        .text("Platform");
+});
+
+
+
+
+
+
+const socialMediaTime = d3.csv("socialMediaTime.csv");
+
+socialMediaTime.then(function(data) {
+    // Convert string values to numbers
+    data.forEach(function(d) {
+        d.AvgLikes = +d.AvgLikes;
+    });
+
+    const margin = {top: 50, right: 50, bottom: 100, left: 70};
+    const width = 800 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const svg = d3.select("#lineplot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Set up scales
+    const xScale = d3.scalePoint()
+        .domain(data.map(d => d.Date))
+        .range([0, width])
+        .padding(0.5);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.AvgLikes) * 1.1])
+        .range([height, 0]);
+
+    // Draw the axis, rotating text to prevent overlap
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("transform", "rotate(-25)");
+
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
+
+    // Add x-axis label
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .style("text-anchor", "middle")
+        .text("Date");
+
+    // Add y-axis label
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 20)
+        .style("text-anchor", "middle")
+        .text("Average Number of Likes");
+
+    // Draw the line with curve
+    const line = d3.line()
+        .x(d => xScale(d.Date))
+        .y(d => yScale(d.AvgLikes))
+        .curve(d3.curveMonotoneX);
+
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "#7b2cbf")  // Change to a vibrant purple
+        .attr("stroke-width", 3)
+        .attr("d", line);
+
+    // Add circles at each data point
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("cx", d => xScale(d.Date))
+        .attr("cy", d => yScale(d.AvgLikes))
+        .attr("r", 6)
+        .attr("fill", "#ff8c00") // Use orange for contrast
+        .attr("stroke", "black")
+        .attr("stroke-width", 1.5)
+        .on("mouseover", function(event, d) {
+            d3.select(this)
+                .transition()
+                .attr("r", 10)
+                .attr("fill", "red");
+        })
+        .on("mouseout", function(event, d) {
+            d3.select(this)
+                .transition()
+                .attr("r", 6)
+                .attr("fill", "#ff8c00");
+        });
+
+});
